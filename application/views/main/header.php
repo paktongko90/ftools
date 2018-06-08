c90724e30639bef03ed7565f8a29e044bd7a497c
paktongko <paktongko90@gmail.com>
2018-06-08 15:56:29 +0800

    dashboard xjadi

--- application/views/main/header.php
+++ application/views/main/header.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>FTools</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-	 <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-	  <a class="navbar-brand" href="#">Logo</a>
-	  	<ul class="navbar-nav">
-	    <li class="nav-item">
-	      <a class="nav-link" href="#">Link</a>
-	    </li>
-	    <li class="nav-item">
-	      <a class="nav-link" href="#">Link</a>
-	    </li>
-	  </ul>
-</nav>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Startmin - Bootstrap Admin Theme</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="<?php echo base_url(); ?>assets/dashboard/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
